28aa18222c067084e209c236bdb72583b998d762
Lau Yee Xuan <yee.xuan.lau@emerson.com>
2025-10-22 13:53:18 +0800

    copy from 861

--- tests/component/_utils.py
+++ tests/component/_utils.py
@@ -7,6 +7,7 @@ from datetime import timezone
 from hightime import datetime as ht_datetime
 from nitypes.time.typing import AnyDateTime
 
+
 def _is_timestamp_close_to_now(timestamp: AnyDateTime, tolerance_seconds: float = 1.0) -> bool:
     current_time = ht_datetime.now(timezone.utc)
     time_diff = abs((timestamp - current_time).total_seconds())
